9f36de9a90a34f1bee5ddcc192d955270ff35ae0
Alexander Strand <alexander@strand.it>
2025-09-05 00:31:06 +0200

    Fixed transfer endpoint to work with database. Added endpoints to get transactions and to create new transactions.

--- app.js
+++ app.js
@@ -49,13 +49,13 @@ app.post('/envelopes', async (req, res, next) => {
     const { category, budget } = req.body
 
     if (!category || !budget) {
-        res.status(400).json({ error: 'You need to add a category and budget to the envelope.' })
+        return res.status(400).json({ error: 'You need to add a category and budget to the envelope.' })
     }
 
     try {
         const results = await db.query(`INSERT INTO envelopes(category, budget) VALUES($1, $2) RETURNING *`, [category, budget])
-
         return res.status(201).json(results.rows[0])
+
     } catch (err) {
         console.error(err);
         return res.status(500).json({ error: 'Internal server error.' })
@@ -67,29 +67,41 @@ app.put('/envelopes/:key/:value', async (req, res, next) => {
     const updatedCategory = req.body.category
     const updatedBudget = req.body.budget
 
-    console.log(`KEY: ${key}  ... VALUE: ${value}`)
+    // console.log(`KEY: ${key}  ... VALUE: ${value}`)
 
     if (!['id', 'category'].includes(key)) {
         return res.status(400).json({ error: 'Invalid lookup key.' });
     }
 
     if (!updatedBudget && !updatedCategory) {
-        res.status(400).json({ error: 'You need to add text to update the envelope budget or category.' })
+        return res.status(400).json({ error: 'You need to add text to update the envelope budget or category.' })
     }
 
     try {
         if (updatedCategory && updatedBudget) {
             const results = await db.query(`UPDATE envelopes SET category = $1, budget = $2 WHERE ${key} = $3 RETURNING *`, [updatedCategory, updatedBudget, value])
+            if (results.rows.length === 0) {
+                return res.status(404).json({ error: `The envelope ${key} does not exist.` });
+            }
 
-            res.status(201).json(results.rows[0])
+            return res.status(201).json(results.rows[0])
+
         } else if (updatedBudget) {
             const results = await db.query(`UPDATE envelopes SET budget = $1 WHERE ${key} = $2 RETURNING *`, [updatedBudget, value])
+            if (results.rows.length === 0) {
+                return res.status(404).json({ error: `The envelope ${key} does not exist.` });
+            }
 
-            res.status(201).json(results.rows[0])
+            return res.status(201).json(results.rows[0])
+
         } else {
             const results = await db.query(`UPDATE envelopes SET category = $1 WHERE ${key} = $2 RETURNING *`, [updatedCategory, value])
+            if (results.rows.length === 0) {
+                return res.status(404).json({ error: `The envelope ${key} does not exist.` });
+            }
 
-            res.status(201).json(results.rows[0])
+            return res.status(201).json(results.rows[0])
+
         }
     } catch (err) {
         console.error(err)
@@ -98,20 +110,35 @@ app.put('/envelopes/:key/:value', async (req, res, next) => {
 }
 )
 
-app.post('/envelopes/:category1/:category2', (req, res, next) => {
-    const category1 = getEnvelopeByCategory(req.params.category1, envelopes)
-    const category2 = getEnvelopeByCategory(req.params.category2, envelopes)
-
+app.post('/envelopes/transfer/:category1/:category2', async (req, res, next) => {
+    const category1 = req.params.category1
+    const category2 = req.params.category2
     const transfer = req.body.transfer
 
-    if (category1 === -1 || category2 === -1) {
-        res.status(404).json({ error: 'One or both of the envelope categories do not exist.' })
-    } else {
-        envelopes[category1].budget -= transfer
-        envelopes[category2].budget += transfer
+    try {
+        await db.query('BEGIN')
 
-        res.status(200).json({ success: `Transfered ${transfer} from ${envelopes[category1].category} to ${envelopes[category2].category}` })
+        const result1 = await db.query('UPDATE envelopes SET budget = budget - $1 WHERE category = $2 RETURNING *', [transfer, category1])
+        const result2 = await db.query('UPDATE envelopes SET budget = budget + $1 WHERE category = $2 RETURNING *', [transfer, category2])
+
+        if (result1.rows.length === 0 || result2.rows.length === 0) {
+            await db.query('ROLLBACK')
+            return res.status(404).json({ error: 'One or both of the envelope categories do not exist.' })
+        }
+
+        await db.query('COMMIT')
+
+        return res.status(201).json({
+            from: result1.rows[0],
+            to: result2.rows[0]
+        })
+
+    } catch (err) {
+        await db.query('ROLLBACK')
+        console.error(err)
+        return res.status(500).json({ error: 'Internal server error.' })
     }
+
 })
 
 app.delete('/envelopes/:id', async (req, res, next) => {
@@ -131,6 +158,75 @@ app.delete('/envelopes/:id', async (req, res, next) => {
         return res.status(500).json({ error: 'Internal server error.' })
     }
 })
+
+// Transactions section ----------------
+
+// READ
+app.get('/transactions', async (req, res) => {
+    try {
+        const results = await db.query('SELECT * FROM transactions')
+        return res.status(200).json(results.rows)
+    } catch (err) {
+        console.error(err)
+        return res.status(500).json({ error: 'Internal server error.' })
+    }
+})
+
+app.get('/transactions/:id', async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const results = await db.query('SELECT * FROM transactions WHERE id = $1', [id])
+
+        if (results.rows.length === 0) {
+            return res.status(404).json({error: 'No transaction with the specified id exists.'})
+        }
+
+    } catch (err) {
+        console.error(err)
+        return res.status(500).json({ error: 'Internal server error.' })
+    }
+})
+
+// CREATE
+app.post('/transactions/:envelope', async (req, res) => {
+    const envelope = req.params.envelope
+    const {amount, recipient} = req.body
+
+        if (!amount || !recipient) {
+        return res.status(400).json({ error: 'You need to add a category and budget to the envelope.' })
+    }
+
+    try {
+        await db.query('BEGIN')
+
+        const getEnvelopeId = await db.query('SELECT id FROM envelopes WHERE category = $1', [envelope])
+
+        const envelopeId = getEnvelopeId.rows[0].id
+
+        const result1 = await db.query('INSERT INTO transactions (amount, recipient, envelope_id) VALUES($1, $2, $3) RETURNING *', [amount, recipient, envelopeId])
+
+        const result2 = await db.query('UPDATE envelopes SET budget = budget - $1 WHERE id = $2 RETURNING *', [amount, envelopeId])
+
+        if (result2.rows.length === 0) {
+            await db.query('ROLLBACK')
+            return res.status(404).json({error: 'That envelope does not exist.'})
+        }
+
+        await db.query('COMMIT')
+
+        return res.status(201).json(result1.rows[0])
+
+    } catch (err) {
+        await db.query('ROLLBACK')
+        console.error(err)
+        return res.status(500).json({ error: 'Internal server error.' })
+    }
+})
+
+// UPDATE
+
+// DELETE
 
 
 
